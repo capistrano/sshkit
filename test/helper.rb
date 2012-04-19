c3e68680aa5cb7ac553e004021d2ff50fc7bc1be
Lee Hambley <lee.hambley@gmail.com>
2012-04-19 21:27:12 +0200

    Configure test environment to force ANSI colours under autotest

--- test/helper.rb
+++ test/helper.rb
@@ -9,6 +9,7 @@ rescue Bundler::BundlerError => e
 end
 require 'minitest/unit'
 require 'mocha'
+require 'turn'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
@@ -17,5 +18,10 @@ require 'deploy'
 class UnitTest < MiniTest::Unit::TestCase
 
 end
+
+#
+# Force colours in Autotest
+#
+Turn.config.ansi = true
 
 MiniTest::Unit.autorun
