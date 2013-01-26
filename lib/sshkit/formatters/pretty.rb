cd28f963b9238fdcc581fab57c9c309939cae5ff
Lee Hambley <lee.hambley@gmail.com>
2013-01-26 16:41:04 +0100

    Pretty formatter honors verbosity (naïvely)

--- lib/sshkit/formatters/pretty.rb
+++ lib/sshkit/formatters/pretty.rb
@@ -8,22 +8,29 @@ module SSHKit
 
       def write(obj)
         if obj.is_a? SSHKit::Command
+
+          return if obj.verbosity < SSHKit.config.output_verbosity
+
           unless obj.started?
             original_output << "[#{c.green(obj.uuid)}] Running #{c.yellow(c.bold(String(obj)))} on #{c.yellow(obj.host.to_s)}\n"
           end
+
           if obj.complete? && !obj.stdout.empty?
             obj.stdout.lines.each do |line|
               original_output << c.green("\t" + line)
             end
           end
+
           if obj.complete? && !obj.stderr.empty?
             obj.stderr.lines.each do |line|
               original_output << c.red("\t" + line)
             end
           end
+
           if obj.finished?
             original_output << "[#{c.green(obj.uuid)}] Finished in #{sprintf('%5.3f seconds', obj.runtime)} command #{c.bold { obj.failure? ? c.red('failed') : c.green('successful') }}.\n"
           end
+
         else
           original_output << c.black(c.on_yellow("Output formatter doesn't know how to handle #{obj.inspect}\n"))
         end
