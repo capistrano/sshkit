cb17ff9c06e5310a682b23521797c211fb5a87fa
Lee Hambley <lee.hambley@gmail.com>
2013-01-27 10:40:59 +0100

    Formatting tweaks

--- lib/sshkit/formatters/pretty.rb
+++ lib/sshkit/formatters/pretty.rb
@@ -12,23 +12,28 @@ module SSHKit
           return if obj.verbosity < SSHKit.config.output_verbosity
 
           unless obj.started?
-            original_output << level(obj) + "[#{c.green(obj.uuid)}] Running #{c.yellow(c.bold(String(obj)))} on #{c.yellow(obj.host.to_s)}\n"
-          end
-
-          if obj.complete? && !obj.stdout.empty?
-            obj.stdout.lines.each do |line|
-              original_output << c.green("\t" + line)
+            original_output << level(obj.verbosity) + uuid(obj) + "Running #{c.yellow(c.bold(String(obj)))} on #{c.yellow(obj.host.to_s)}\n"
+            if SSHKit.config.output_verbosity = Logger::DEBUG
+              original_output << level(Logger::DEBUG) + uuid(obj) + c.white("Command: #{c.blue(obj.to_command)}") + "\n"
             end
           end
 
-          if obj.complete? && !obj.stderr.empty?
-            obj.stderr.lines.each do |line|
-              original_output << c.red("\t" + line)
+          if SSHKit.config.output_verbosity == Logger::DEBUG
+            if obj.complete? && !obj.stdout.empty?
+              obj.stdout.lines.each do |line|
+                original_output << level(Logger::DEBUG) + uuid(obj) + c.green("\t" + line)
+              end
+            end
+
+            if obj.complete? && !obj.stderr.empty?
+              obj.stderr.lines.each do |line|
+                original_output << level(Logger::DEBUG) + uuid(obj) + c.red("\t" + line)
+              end
             end
           end
 
           if obj.finished?
-            original_output << level(obj) + "[#{c.green(obj.uuid)}] Finished in #{sprintf('%5.3f seconds', obj.runtime)} command #{c.bold { obj.failure? ? c.red('failed') : c.green('successful') }}.\n"
+            original_output << level(obj.verbosity) + uuid(obj) + "Finished in #{sprintf('%5.3f seconds', obj.runtime)} command #{c.bold { obj.failure? ? c.red('failed') : c.green('successful') }}.\n"
           end
 
         else
@@ -43,10 +48,14 @@ module SSHKit
         @c ||= Term::ANSIColor
       end
 
-      def level(obj)
+      def uuid(obj)
+        "[#{c.green(obj.uuid)}] "
+      end
+
+      def level(verbosity)
         # Insane number here accounts for the control codes added
         # by term-ansicolor
-        sprintf "%14s ", c.send(level_formatting(obj.verbosity), level_names(obj.verbosity))
+        sprintf "%14s ", c.send(level_formatting(verbosity), level_names(verbosity))
       end
 
       def level_formatting(level_num)
