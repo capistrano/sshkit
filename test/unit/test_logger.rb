d5d061d68a765bbc92a4fed05a9653193f20b80b
Rafa García <contacto@rafagarcia.net>
2013-05-14 16:24:51 +0200

    Add and include Logger with severity constants
    It makes test be green again :)

---
+++ test/unit/test_logger.rb
@@ -0,0 +1,18 @@
+require 'helper'
+
+module SSHKit
+
+  class TestLogger < UnitTest
+
+    def test_logger_severity_constants
+      assert_equal Logger::TRACE, -1
+      assert_equal Logger::DEBUG,  0
+      assert_equal Logger::INFO,   1
+      assert_equal Logger::WARN,   2
+      assert_equal Logger::ERROR,  3
+      assert_equal Logger::FATAL,  4
+    end
+
+  end
+
+end
