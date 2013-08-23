f8aba5f5aac40813a7eb65bf21cabad35c22e10e
seenmyfate <seenmyfate@gmail.com>
2013-08-23 15:48:12 +0100

    remove duplicated runners

--- lib/sshkit/runners/parallel.rb
+++ lib/sshkit/runners/parallel.rb
@@ -4,23 +4,6 @@ module SSHKit
 
   module Runner
 
-    class Abstract
-
-      attr_reader :hosts, :block
-
-      def initialize(hosts, &block)
-        @hosts       = Array(hosts)
-        @block       = block
-      end
-
-      private
-
-      def backend(host, &block)
-        SSHKit.config.backend.new(host, &block)
-      end
-
-    end
-
     class Parallel < Abstract
       def execute
         threads = []
@@ -30,34 +13,6 @@ module SSHKit
           end
         end
         threads.map(&:join)
-      end
-    end
-
-    class Sequential < Abstract
-      attr_writer :wait_interval
-      def execute
-        hosts.each do |host|
-          backend(host, &block).run
-          sleep wait_interval
-        end
-      end
-      private
-      def wait_interval
-        @wait_interval ||= 2
-      end
-    end
-
-    class Group < Sequential
-      attr_writer :group_size
-      def execute
-        hosts.each_slice(group_size).collect do |group_hosts|
-          Parallel.new(group_hosts, &block).execute
-          sleep wait_interval
-        end.flatten
-      end
-      private
-      def group_size
-        @group_size ||= 2
       end
     end
 
