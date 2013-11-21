3da496f5714d15cbee79e97d250cf4288556588a
chqr <choover@kayak.com>
2013-11-21 14:55:13 -0500

    Quick fix for issue 51. Moved upload, download logging logic into shared method, added unit test.
    Note: although it no longer fails when downloading an empty file, it
    will log the empty warning twice...

--- test/unit/backends/test_netssh.rb
+++ test/unit/backends/test_netssh.rb
@@ -27,6 +27,25 @@ module SSHKit
         assert_equal false,                       backend.config.ssh_options[:forward_agent]
         assert_equal %w(publickey password),      backend.config.ssh_options[:auth_methods]
       end
+
+      def test_transfer_summarizer
+        netssh = Netssh.new(Host.new('fake'))
+
+        summarizer = netssh.send(:transfer_summarizer,'Transferring')
+
+        [
+         [1,    100, :debug, 'Transferring afile 1.0%'],
+         [1,    3,   :debug, 'Transferring afile 33.33%'],
+         [0,    1,   :debug, 'Transferring afile 0.0%'],
+         [1,    2,   :info,  'Transferring afile 50.0%'],
+         [0,    0,   :warn,  'percentage 0/0'],
+         [1023, 343, :debug, 'Transferring'],
+        ].each do |transferred,total,method,substring|
+          netssh.expects(method).with { |msg| msg.include?(substring) }
+          summarizer.call(nil,'afile',transferred,total)
+        end
+      end
+
     end
   end
 end
