9a3c3888985c206bb4380d2c24a628f8b631dda2
Rob Dupuis <robd@robd.co.uk>
2015-04-21 23:46:49 +0100

    Added command lifecycle logging test for pretty formatter
    Try to protect against duplicate log lines caused by the interaction between the backend, command and formatter

--- test/unit/formatters/test_pretty.rb
+++ test/unit/formatters/test_pretty.rb
@@ -47,5 +47,34 @@ module SSHKit
       assert_equal "\e[0;30;49mDEBUG\e[0m Test\n", output
     end
 
+    def test_command_lifecycle_logging
+      command = fixed_uid_command('aaaaaa', :a_cmd, 'some args', 'localhost')
+      pretty << command
+      command.started = true
+      pretty << command
+      command.stdout = 'stdout message'
+      pretty << command
+      command.stderr = 'stderr message'
+      pretty << command
+      command.exit_status = 0
+      pretty << command
+
+      expected_log_lines = [
+        "\e[0;34;49mINFO\e[0m [\e[0;32;49maaaaaa\e[0m] Running \e[1;33;49m/usr/bin/env a_cmd some args\e[0m on \e[0;34;49mlocalhost\e[0m",
+        "\e[0;30;49mDEBUG\e[0m [\e[0;32;49maaaaaa\e[0m] Command: \e[0;34;49m/usr/bin/env a_cmd some args\e[0m",
+        "\e[0;30;49mDEBUG\e[0m [\e[0;32;49maaaaaa\e[0m] \e[0;32;49m\tstdout message\e[0m",
+        "\e[0;30;49mDEBUG\e[0m [\e[0;32;49maaaaaa\e[0m] \e[0;31;49m\tstderr message\e[0m",
+        "\e[0;34;49mINFO\e[0m [\e[0;32;49maaaaaa\e[0m] Finished in 0.000 seconds with exit status 0 (\e[1;32;49msuccessful\e[0m)."
+      ]
+      assert_equal expected_log_lines, output.split("\n")
+    end
+
+    private
+
+    def fixed_uid_command(constant_uuid, *args, host)
+      command = SSHKit::Command.new(*args, host: Host.new(host))
+      command.stubs(:uuid).returns(constant_uuid)
+      command
+    end
   end
 end
