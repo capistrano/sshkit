ea71dd5cc573618bd13679cd1e593b55b3a5db70
Rob Dupuis <robd@robd.co.uk>
2015-05-05 16:14:52 +0100

    Added support for regex hash keys and procs to interaction_handlers

--- lib/sshkit/mapping_interaction_handler.rb
+++ lib/sshkit/mapping_interaction_handler.rb
@@ -3,7 +3,17 @@ module SSHKit
   class MappingInteractionHandler
 
     def initialize(mapping)
-      @mapping = mapping
+      @mapping_proc = case mapping
+        when Hash
+          lambda do |server_output|
+            first_matching_key_value = mapping.find { |k, _v| k === server_output }
+            first_matching_key_value.nil? ? nil : first_matching_key_value.last
+          end
+        when Proc
+          mapping
+        else
+          raise "Unsupported mapping type: #{mapping.class} - only Hash and Proc mappings are supported"
+      end
     end
 
     def on_stdout(channel, data, command)
@@ -21,16 +31,18 @@ module SSHKit
 
       output.debug("Looking up response for #{stream_name} message #{data.inspect}")
 
-      output.warn("Unable to find interaction handler mapping for #{stream_name}: #{data.inspect} so no response was sent") unless @mapping.key?(data)
+      response_data = @mapping_proc.call(data)
 
-      unless (response_data = @mapping[data]).nil?
+      if response_data.nil?
+        output.debug("Unable to find interaction handler mapping for #{stream_name}: #{data.inspect} so no response was sent")
+      else
         output.debug("Sending #{response_data.inspect}")
         if channel.respond_to?(:send_data) # Net SSH Channel
           channel.send_data(response_data)
         elsif channel.respond_to?(:write) # Local IO
           channel.write(response_data)
         else
-          raise 'Unable to write response data to channel - unrecognised channel type'
+          raise "Unable to write response data to channel #{channel.inspect} - does not support 'send_data' or 'write'"
         end
       end
     end
--- test/unit/test_mapping_interaction_handler.rb
+++ test/unit/test_mapping_interaction_handler.rb
@@ -21,19 +21,74 @@ module SSHKit
     end
 
     def test_calls_send_data_with_mapped_input_when_stderr_matches
-      handler = MappingInteractionHandler.new('Server output' => "some input\n")
-
       channel.expects(:send_data).with("some input\n")
 
-      handler.on_stderr(channel, 'Server output', nil)
+      MappingInteractionHandler.new('Server output' => "some input\n").on_stderr(channel, 'Server output', nil)
     end
 
     def test_raises_warning_if_server_output_is_not_matched
-      handler = MappingInteractionHandler.new({})
+      @output.expects(:debug).with('Unable to find interaction handler mapping for stdout: "Server output\n" so no response was sent')
 
-      @output.expects(:warn).with('Unable to find interaction handler mapping for stdout: "Server output\n" so no response was sent')
+      MappingInteractionHandler.new({}).on_stdout(channel, "Server output\n", nil)
+    end
 
-      handler.on_stdout(channel, "Server output\n", nil)
+    def test_does_not_warn_for_supports_mapping_to_nil
+      MappingInteractionHandler.new({"Some output\n" => nil}).on_stdout(channel, "Some output\n", nil)
+    end
+
+    def test_supports_regex_keys
+      channel.expects(:send_data).with("Input\n")
+
+      MappingInteractionHandler.new({ /Some \w+ output\n/ => "Input\n"}).on_stdout(channel, "Some lovely output\n", nil)
+    end
+
+    def test_supports_lambda_mapping
+      channel.expects(:send_data).with("GREAT Input\n")
+
+      mapping = lambda do |server_output|
+        case server_output
+        when /Some (\w+) output\n/
+          "#{$1.upcase} Input\n"
+        end
+      end
+
+      MappingInteractionHandler.new(mapping).on_stdout(channel, "Some great output\n", nil)
+    end
+
+
+    def test_matches_keys_in_ofer
+      interaction_handler = MappingInteractionHandler.new({
+        "Specific output\n" => "Specific Input\n",
+        /.*/ => "Default Input\n"
+      })
+
+      channel.expects(:send_data).with("Specific Input\n")
+      interaction_handler.on_stdout(channel, "Specific output\n", nil)
+    end
+
+    def test_supports_default_mapping
+      interaction_handler = MappingInteractionHandler.new({
+        "Specific output\n" => "Specific Input\n",
+        /.*/ => "Default Input\n"
+      })
+
+      channel.expects(:send_data).with("Specific Input\n")
+      interaction_handler.on_stdout(channel, "Specific output\n", nil)
+    end
+
+    def test_raises_for_unsupported_mapping_type
+      raised_error = assert_raises RuntimeError do
+        MappingInteractionHandler.new(Object.new)
+      end
+      assert_equal('Unsupported mapping type: Object - only Hash and Proc mappings are supported', raised_error.message)
+    end
+
+    def test_raises_for_unsupported_channel_type
+      handler = MappingInteractionHandler.new({"Some output\n" => "Whatever"})
+      raised_error = assert_raises RuntimeError do
+        handler.on_stdout(Object.new, "Some output\n", nil)
+      end
+      assert_match(/Unable to write response data to channel #<Object:.*> - does not support 'send_data' or 'write'/, raised_error.message)
     end
   end
 
