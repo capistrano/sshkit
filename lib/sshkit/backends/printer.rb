5124acfd148aacaa5f6976daef1f52650a40ce34
Rob Dupuis <robd@robd.co.uk>
2015-05-09 00:33:59 +0100

    Added test for printer backend

--- lib/sshkit/backends/printer.rb
+++ lib/sshkit/backends/printer.rb
@@ -1,14 +1,15 @@
 module SSHKit
   module Backend
 
+    # Printer is used to implement --dry-run in Capistrano
     class Printer < Abstract
 
       def execute_command(cmd)
         output << cmd
       end
+
       alias :upload! :execute
       alias :download! :execute
-      alias :test :execute
     end
   end
 end
