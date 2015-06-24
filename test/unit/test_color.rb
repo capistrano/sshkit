066c4a3563873d6229f7454458f2d6a0ec760660
Matt Brictson <matt@mattbrictson.com>
2015-06-24 14:41:32 -0700

    Add tests for all supported colors and bold

--- test/unit/test_color.rb
+++ test/unit/test_color.rb
@@ -26,5 +26,45 @@ module SSHKit
       color = SSHKit::Color.new(stub(), {})
       assert_equal 'hi', color.colorize('hi', :red)
     end
+
+    def test_colorize_colors
+      color = SSHKit::Color.new(stub(tty?: true), {})
+      assert_equal "\e[0;30;49mhi\e[0m", color.colorize('hi', :black)
+      assert_equal "\e[0;31;49mhi\e[0m", color.colorize('hi', :red)
+      assert_equal "\e[0;32;49mhi\e[0m", color.colorize('hi', :green)
+      assert_equal "\e[0;33;49mhi\e[0m", color.colorize('hi', :yellow)
+      assert_equal "\e[0;34;49mhi\e[0m", color.colorize('hi', :blue)
+      assert_equal "\e[0;35;49mhi\e[0m", color.colorize('hi', :magenta)
+      assert_equal "\e[0;36;49mhi\e[0m", color.colorize('hi', :cyan)
+      assert_equal "\e[0;37;49mhi\e[0m", color.colorize('hi', :white)
+      assert_equal "\e[0;90;49mhi\e[0m", color.colorize('hi', :light_black)
+      assert_equal "\e[0;91;49mhi\e[0m", color.colorize('hi', :light_red)
+      assert_equal "\e[0;92;49mhi\e[0m", color.colorize('hi', :light_green)
+      assert_equal "\e[0;93;49mhi\e[0m", color.colorize('hi', :light_yellow)
+      assert_equal "\e[0;94;49mhi\e[0m", color.colorize('hi', :light_blue)
+      assert_equal "\e[0;95;49mhi\e[0m", color.colorize('hi', :light_magenta)
+      assert_equal "\e[0;96;49mhi\e[0m", color.colorize('hi', :light_cyan)
+      assert_equal "\e[0;97;49mhi\e[0m", color.colorize('hi', :light_white)
+    end
+
+    def test_colorize_bold_colors
+      color = SSHKit::Color.new(stub(tty?: true), {})
+      assert_equal "\e[1;30;49mhi\e[0m", color.colorize('hi', :black, :bold)
+      assert_equal "\e[1;31;49mhi\e[0m", color.colorize('hi', :red, :bold)
+      assert_equal "\e[1;32;49mhi\e[0m", color.colorize('hi', :green, :bold)
+      assert_equal "\e[1;33;49mhi\e[0m", color.colorize('hi', :yellow, :bold)
+      assert_equal "\e[1;34;49mhi\e[0m", color.colorize('hi', :blue, :bold)
+      assert_equal "\e[1;35;49mhi\e[0m", color.colorize('hi', :magenta, :bold)
+      assert_equal "\e[1;36;49mhi\e[0m", color.colorize('hi', :cyan, :bold)
+      assert_equal "\e[1;37;49mhi\e[0m", color.colorize('hi', :white, :bold)
+      assert_equal "\e[1;90;49mhi\e[0m", color.colorize('hi', :light_black, :bold)
+      assert_equal "\e[1;91;49mhi\e[0m", color.colorize('hi', :light_red, :bold)
+      assert_equal "\e[1;92;49mhi\e[0m", color.colorize('hi', :light_green, :bold)
+      assert_equal "\e[1;93;49mhi\e[0m", color.colorize('hi', :light_yellow, :bold)
+      assert_equal "\e[1;94;49mhi\e[0m", color.colorize('hi', :light_blue, :bold)
+      assert_equal "\e[1;95;49mhi\e[0m", color.colorize('hi', :light_magenta, :bold)
+      assert_equal "\e[1;96;49mhi\e[0m", color.colorize('hi', :light_cyan, :bold)
+      assert_equal "\e[1;97;49mhi\e[0m", color.colorize('hi', :light_white, :bold)
+    end
   end
 end
