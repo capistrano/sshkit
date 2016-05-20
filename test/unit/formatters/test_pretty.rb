f97649d30532734d4ae38e5bd200364f549e10df
kevin.burk <kevin.burk@workday.com>
2016-05-20 13:38:57 -0700

    Fix colorized log alignment.

--- test/unit/formatters/test_pretty.rb
+++ test/unit/formatters/test_pretty.rb
@@ -18,12 +18,12 @@ module SSHKit
     end
 
     {
-      log:   "\e[0;34;49mINFO\e[0m Test\n",
-      fatal: "\e[0;31;49mFATAL\e[0m Test\n",
-      error: "\e[0;31;49mERROR\e[0m Test\n",
-      warn:  "\e[0;33;49mWARN\e[0m Test\n",
-      info:  "\e[0;34;49mINFO\e[0m Test\n",
-      debug: "\e[0;30;49mDEBUG\e[0m Test\n"
+      log:   "\e[0;34;49m  INFO\e[0m Test\n",
+      fatal: "\e[0;31;49m FATAL\e[0m Test\n",
+      error: "\e[0;31;49m ERROR\e[0m Test\n",
+      warn:  "\e[0;33;49m  WARN\e[0m Test\n",
+      info:  "\e[0;34;49m  INFO\e[0m Test\n",
+      debug: "\e[0;30;49m DEBUG\e[0m Test\n"
     }.each do |level, expected_output|
       define_method("test_#{level}_output_with_color") do
         output.stubs(:tty?).returns(true)
@@ -37,11 +37,11 @@ module SSHKit
       simulate_command_lifecycle(pretty)
 
       expected_log_lines = [
-        "\e[0;34;49mINFO\e[0m [\e[0;32;49maaaaaa\e[0m] Running \e[1;33;49m/usr/bin/env a_cmd some args\e[0m as \e[0;34;49muser\e[0m@\e[0;34;49mlocalhost\e[0m",
-        "\e[0;30;49mDEBUG\e[0m [\e[0;32;49maaaaaa\e[0m] Command: \e[0;34;49m/usr/bin/env a_cmd some args\e[0m",
-        "\e[0;30;49mDEBUG\e[0m [\e[0;32;49maaaaaa\e[0m] \e[0;32;49m\tstdout message\e[0m",
-        "\e[0;30;49mDEBUG\e[0m [\e[0;32;49maaaaaa\e[0m] \e[0;31;49m\tstderr message\e[0m",
-        "\e[0;34;49mINFO\e[0m [\e[0;32;49maaaaaa\e[0m] Finished in 1.000 seconds with exit status 0 (\e[1;32;49msuccessful\e[0m)."
+        "\e[0;34;49m  INFO\e[0m [\e[0;32;49maaaaaa\e[0m] Running \e[1;33;49m/usr/bin/env a_cmd some args\e[0m as \e[0;34;49muser\e[0m@\e[0;34;49mlocalhost\e[0m",
+        "\e[0;30;49m DEBUG\e[0m [\e[0;32;49maaaaaa\e[0m] Command: \e[0;34;49m/usr/bin/env a_cmd some args\e[0m",
+        "\e[0;30;49m DEBUG\e[0m [\e[0;32;49maaaaaa\e[0m] \e[0;32;49m\tstdout message\e[0m",
+        "\e[0;30;49m DEBUG\e[0m [\e[0;32;49maaaaaa\e[0m] \e[0;31;49m\tstderr message\e[0m",
+        "\e[0;34;49m  INFO\e[0m [\e[0;32;49maaaaaa\e[0m] Finished in 1.000 seconds with exit status 0 (\e[1;32;49msuccessful\e[0m)."
       ]
       assert_equal expected_log_lines, output.split("\n")
     end
