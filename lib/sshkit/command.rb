af8d1dd4b9fb735c4b04aee9d935193f411e740a
pblesi <code@getbraintree.com>
2018-05-15 10:51:01 -0500

    fixup! Fix escaped quotes bug in Command#group

--- lib/sshkit/command.rb
+++ lib/sshkit/command.rb
@@ -182,7 +182,7 @@ module SSHKit
 
     def group(&_block)
       return yield unless options[:group]
-      "sg #{options[:group]} -c \"%s\"" % %Q{#{yield}}
+      %Q(sg #{options[:group]} -c "#{yield}")
       # We could also use the so-called heredoc format perhaps:
       #"newgrp #{options[:group]} <<EOC \\\"%s\\\" EOC" % %Q{#{yield}}
     end
